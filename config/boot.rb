9705e02a3ac83cf45de00dc37b926deed32e4fca
Yoshiyuki Hirano <yhirano@me.com>
2018-02-03 06:43:33 +0900

    Configure bootsnap

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,8 @@
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+
+if %w[s server].any? { |a| ARGV.include?(a) }
+  $stdout.puts "=> Booting Rails"
+end
