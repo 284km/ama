80dd7e77891cebf43c61712725ce977f821f87c9
Yoshiyuki Hirano <yhirano@me.com>
2018-02-03 06:52:10 +0900

    Fix rubocop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-   config.webpacker.check_yarn_integrity = false  # Settings specified here will take precedence over those in config/application.rb.
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -78,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Settings specified here will take precedence over those in config/application.rb.
+  config.webpacker.check_yarn_integrity = false
 end
