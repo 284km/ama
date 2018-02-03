bcfb69d75a735cba77758d23a2a91e3c679eb738
Yoshiyuki Hirano <yhirano@me.com>
2018-02-03 11:16:28 +0900

    Impl UserDecorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,2 +1,9 @@
 module UserDecorator
+  def gravatar_url
+    @gravatar_url ||= "https://www.gravatar.com/avatar/#{gravatar_id}"
+  end
+
+  def gravatar_id
+    Digest::MD5.hexdigest(email.downcase)
+  end
 end
--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,9 +1,31 @@
 require "rails_helper"
 
 describe UserDecorator do
-  subject { user }
+  describe "decorate" do
+    subject { user }
 
-  let(:user) { User.new.extend UserDecorator }
+    let(:user) { User.new.extend UserDecorator }
 
-  it { should be_a User }
+    it { should be_a User }
+  end
+
+  describe "#gravatar_id" do
+    before { user.extend UserDecorator }
+
+    subject { user.gravatar_id }
+
+    let(:user) { build(:user) }
+
+    it { is_expected.to eq Digest::MD5.hexdigest(user.email.downcase) }
+  end
+
+  describe "#gravatar_url" do
+    before { user.extend UserDecorator }
+
+    subject { user.gravatar_url }
+
+    let(:user) { build(:user) }
+
+    it { is_expected.to eq "https://www.gravatar.com/avatar/#{user.gravatar_id}" }
+  end
 end
