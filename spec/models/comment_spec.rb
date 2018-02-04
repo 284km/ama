9ad8f546d7a29951d82e34c4d0d85b9a2a37fa08
Yoshiyuki Hirano <yhirano@me.com>
2018-02-05 05:41:42 +0900

    Impl Comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id          :integer          not null, primary key
+#  topic_id    :integer          not null
+#  user_id     :integer          not null
+#  content     :text             not null
+#  likes_count :integer          default(0), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_comments_on_topic_id  (topic_id)
+#  index_comments_on_user_id   (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (topic_id => topics.id)
+#  fk_rails_...  (user_id => users.id)
+#
+
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "factory" do
+    subject { build(:comment) }
+
+    it { is_expected.to be_valid }
+  end
 end
