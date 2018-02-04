a4896e1508221091c8dc52652dccd343f551ddb5
Yoshiyuki Hirano <yhirano@me.com>
2018-02-05 03:16:32 +0900

    Impl Topic

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -1,8 +1,28 @@
+# == Schema Information
+#
+# Table name: topics
+#
+#  id             :integer          not null, primary key
+#  user_id        :integer          not null
+#  title          :string           not null
+#  description    :text             not null
+#  comments_count :integer          default(0), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+# Indexes
+#
+#  index_topics_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
 FactoryBot.define do
   factory :topic do
-    user nil
-    title "MyString"
-    description "MyText"
-    comments_count 1
+    association :user
+    title       { Faker::Lorem.sentence  }
+    description { Faker::Lorem.paragraph }
   end
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,5 +1,30 @@
+# == Schema Information
+#
+# Table name: topics
+#
+#  id             :integer          not null, primary key
+#  user_id        :integer          not null
+#  title          :string           not null
+#  description    :text             not null
+#  comments_count :integer          default(0), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+# Indexes
+#
+#  index_topics_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
 require "rails_helper"
 
 RSpec.describe Topic, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "factory" do
+    subject { build(:topic) }
+
+    it { is_expected.to be_valid }
+  end
 end
