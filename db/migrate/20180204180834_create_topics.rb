eadf1aa3bc697fc24dbba1dca821c8298f0a10bc
Yoshiyuki Hirano <yhirano@me.com>
2018-02-05 03:10:23 +0900

    Create topics table

--- db/migrate/20180204180834_create_topics.rb
+++ db/migrate/20180204180834_create_topics.rb
@@ -1,10 +1,10 @@
 class CreateTopics < ActiveRecord::Migration[5.1]
   def change
     create_table :topics do |t|
-      t.references :user, foreign_key: true
-      t.string :title
-      t.text :description
-      t.integer :comments_count
+      t.references :user, foreign_key: true, null: false
+      t.string  :title,          null: false
+      t.text    :description,    null: false
+      t.integer :comments_count, null: false, default: 0
 
       t.timestamps
     end
