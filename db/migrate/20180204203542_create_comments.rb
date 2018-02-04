91ca9a09e30b389b5197776c5155830a78d4f056
Yoshiyuki Hirano <yhirano@me.com>
2018-02-05 05:37:42 +0900

    Create comments table

--- db/migrate/20180204203542_create_comments.rb
+++ db/migrate/20180204203542_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-      t.references :topic, foreign_key: true
-      t.references :user, foreign_key: true
-      t.text :content
-      t.integer :likes_count
+      t.references :topic, foreign_key: true, null: false
+      t.references :user,  foreign_key: true, null: false
+      t.text    :content,     null: false
+      t.integer :likes_count, null: false, default: 0
 
       t.timestamps
     end
